9df39933ab68383c86152cf750131d3029d2a115
Kang Ye (叶康) <kangye@quarkfinance.com>
2017-09-13 11:57:43 +0800

    加入theme测试

--- src/components/DynamicFormRender.js
+++ src/components/DynamicFormRender.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Form,Spin } from 'antd';
 import _ from 'lodash';
+import '../theme/theme.less';
 
 import {
     loadFormDefinition,
